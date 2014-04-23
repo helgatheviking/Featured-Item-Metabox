8d47bb921924558541af3ab39395f0be6c4e8266
Kathy Darling <helgatheviking@gmail.com>
2014-04-23 12:11:42 +0200

    add minified script

--- inc/class.Featured_Item_Metabox.php
+++ inc/class.Featured_Item_Metabox.php
@@ -354,7 +354,9 @@ class Featured_Item_Metabox {
     	if ( $screen->base != "edit" ||  ! isset( $options['types'] ) || ! in_array( $screen->post_type, $options ) ) 
     		return;
 
-      wp_enqueue_script( 'featured-item', plugins_url( 'js/featureditem.js', dirname(__FILE__) ), array( 'jquery' ), null, true );
+    	$suffix = defined( WP_SCRIPT_DEBUG )  && WP_SCRIPT_DEBUG ? '' : '.min';
+
+		wp_enqueue_script( 'featured-item', plugins_url( 'js/featureditem' . $suffix . '.js', dirname(__FILE__) ), array( 'jquery' ), null, true );
 
     }
 
