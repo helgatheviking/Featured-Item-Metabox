e99b55afad04462000189e1092e630708d2cb60c
Kathy Darling <helgatheviking@gmail.com>
2015-09-25 12:17:51 +0700

    add text domain to header

--- featured-item-metabox.php
+++ featured-item-metabox.php
@@ -7,22 +7,22 @@ Version: 1.2.4
 Author: Kathy Darling
 Author URI: http://www.kathyisawesome.com
 License: GPL2
+Text Domain: featured-item-metabox
 
-		Copyright 2015  Kathy Darling  (email: kathy.darling@gmail.com)
+Copyright 2015  Kathy Darling  (email: kathy.darling@gmail.com)
 
-		This program is free software; you can redistribute it and/or modify
-		it under the terms of the GNU General Public License, version 2, as
-		published by the Free Software Foundation.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as
+published by the Free Software Foundation.
 
-		This program is distributed in the hope that it will be useful,
-		but WITHOUT ANY WARRANTY; without even the implied warranty of
-		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-		GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-		You should have received a copy of the GNU General Public License
-		along with this program; if not, write to the Free Software
-		Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 
